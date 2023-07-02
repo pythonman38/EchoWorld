aa38df7bdcf624c3b7c324b23a9ff244252d7dfd
Michael Walker <pythonk@email.phoenix.edu>
2023-07-01 22:19:12 -0400

    Echo World Game Setup - Step 1
    Created a blank game in Unreal Engine using a blank map with C++ code supported. Added all anticipated modules that will be used in the game to the build file. Will start creating supporting classes in the next commit.

---
+++ EchoWorld.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EchoWorld.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EchoWorld, "EchoWorld" );
---
+++ README.md
@@ -0,0 +1,2 @@
+# Echo's World - Sword Wielding Third Person
+Third person character game with sword wielding main character. Created with Unreal Engine 5.2 and Visual Studios 2022.
