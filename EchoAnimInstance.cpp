79b1e57fdedcf34e9be342b85622528fad58a81d
Michael Walker <pythonk@email.phoenix.edu>
2023-07-03 21:09:08 -0400

    The Animation Blueprint - Step 4
    Created an Anim Instance Class (EchoAnimInstance) to use as a parent class for the animation blueprint (ABP_Echo). So far we just have basic movement including idle, running and jumping. Also built a Control Rig so that the character's feet look natural when standing on inclines. The the knees bend so the feet can adjust to different angles of the surface that the character is standing on. Not a lot of C++ code for this commit but Control Rigs do take a LOT of node sin the blueprint!

---
+++ EchoAnimInstance.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EchoAnimInstance.h"
+#include "EchoCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void UEchoAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	EchoCharacter = Cast<AEchoCharacter>(TryGetPawnOwner());
+}
+
+void UEchoAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	if (EchoCharacter == nullptr) EchoCharacter = Cast<AEchoCharacter>(TryGetPawnOwner());
+
+	if (EchoCharacter)
+	{
+		// Get the lateral speed of the character from velocity
+		Speed = UKismetMathLibrary::VSizeXY(EchoCharacter->GetCharacterMovement()->Velocity);
+
+		// Is the character in the air?
+		bIsInAir = EchoCharacter->GetCharacterMovement()->IsFalling();
+
+		// Is the character moving?
+		bIsMoving = EchoCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0;
+	}
+}
---
+++ EchoAnimInstance.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "EchoAnimInstance.generated.h"
+
+class AEchoCharacter;
+class UCharacterMovementComponent;
+
+UCLASS()
+class ECHOWORLD_API UEchoAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+
+	UFUNCTION(BlueprintCallable)
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<AEchoCharacter> EchoCharacter;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	bool bIsMoving;
+};
